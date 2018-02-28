55ddc1e6aa6964b96aebc1e2498c22b637bd546e
Willian Kirsch <kirschw@hotmail.com>
2018-02-27 21:07:55 -0300

    Estrutura

---
+++ src/components/Cabecalho.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import AppBar from 'material-ui/AppBar';
+
+export default class Cabecalho extends Component {
+    render() {
+        return (
+            <AppBar
+                title="Title"
+                iconClassNameRight="muidocs-icon-navigation-expand-more"
+                />
+        );
+    }
+}
